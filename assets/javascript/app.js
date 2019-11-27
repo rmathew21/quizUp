fc2beb23bf0dc81a4c0dc53f336a3caf9dbb93fd
rob <robmathew21@gmail.com>
2019-11-27 00:29:01 -0600

    added CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -103,6 +103,74 @@ let game = {
 
         $("#counter-number").text(this.counter);
 
-        card.append
+        card.append("<h3>Correct Answers: " + this.correct + "<h3>");
+        card.append("<h3>Incorrect Answers: " + this.incorrect + "<h3>");
+        card.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "<h3>");
+        card.append("<br><button id+'start-over'>Start Over?</button>");
+    },
+
+    clicked: function (e) {
+        clearInterval(window.timer);
+        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+            this.answeredCorrectly();
+        }
+        else {
+            this.answeredIncorrectly();
+        }
+    },
+
+    answeredIncorrectly: function () {
+
+        this.incorrect++;
+
+        clearInterval(window.timer);
+
+        card.html("<h2>Nope!</h2>");
+        card.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer + "</h3>");
+        card.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+        if (this.currentQuestion === questions.length - 1) {
+            setTimeout(this.results.bind(this), 3 * 1000);
+        }
+        else {
+            setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+        }
+    },
+
+    answeredCorrectly: function () {
+        clearInterval(window.timer);
+
+        this.correct++;
+
+        card.html("<h2>Correct!<h2>");
+        card.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+        if (this.currentQuestion === questions.length - 1) {
+            setTimeout(this.results.bind(this), 3 * 1000);
+        }
+        else {
+            setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+        }
+    },
+
+    reset: function () {
+        this.currentQuestion = 0;
+        this.counter = countStartNumber;
+        this.correct = 0;
+        this.incorrect = 0;
+        this.loadQuestion();
     }
-}
+};
+
+// Click Events
+
+$(document).on("click", "#start-over", game.reset.bind(game));
+
+$(document).on("click", ".answer-button", function (e) {
+    game.clicked.bind(game, e)();
+});
+
+$(document).on("click", "#start", function () {
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+    game.loadQuestion.bind(game)();
+});
